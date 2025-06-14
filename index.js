60c4713e72fabdebdbe501b134c81d19bbb11072
sachindumalshan <sachindu.info@gmail.com>
2025-06-14 14:29:42 +0530

    docker file updated

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const __dirname = path.dirname(__filename);
 
 const server = http.createServer((req, res) => {
     const filePath = path.join(__dirname, "message.txt");
-
     fs.readFile(filePath, "utf8", (err, data) => {
         if (err) {
             res.writeHead(500, { "Content-Type": "text/plain" });
@@ -20,6 +19,7 @@ const server = http.createServer((req, res) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+// Listen on port 5000 and bind to all interfaces (0.0.0.0)
+server.listen(5000, '0.0.0.0', () => {
+    console.log("Server is running on port 5000");
 });
